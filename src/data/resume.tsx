4b771c4d6f4f5c14f1dfd2b2d31726a5c695da18
jatin-agrawal17 <mittaljatin441@gmail.com>
2026-06-10 07:28:57 +0530

    project

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -19,7 +19,7 @@ import { packages } from "./data.packages";
 export const DATA = {
   name: "Jatin Agrawal",
   initials: "JA",
-  url: "https://portfolio.bhdocs.in",
+  url: "http://localhost:3000",
   location: "Jaipur, India",
   locationLink: "https://www.google.com/maps/place/jaipur",
 description:
